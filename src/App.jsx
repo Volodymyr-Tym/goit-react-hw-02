fa86311828d71d412b67334c1183ff3669e23627
Volodymyr-Tym <163361581+Volodymyr-Tym@users.noreply.github.com>
2024-09-26 15:57:31 +0100

    add some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import './App.css';
+import { useState } from 'react';
+
+import Description from './components/Description/Description';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
 
 function App() {
+  const [rating, setRating] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = feedbackType => {
+    setRating({ ...rating, [feedbackType]: rating[feedbackType] + 1 });
+
+    const reset = { good: 0, neutral: 0, bad: 0 };
+    feedbackType === 'reset' && setRating({ ...reset });
+  };
+
+  const totalFeedback = rating.good + rating.neutral + rating.bad;
+
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedbacks={rating}></Feedback>
+      ) : (
+        <Notification></Notification>
+      )}
+    </div>
   );
 }
 
