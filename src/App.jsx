186d3266a74e5b28f0a6368195dd242a8b1c35da
Volodymyr-Tym <163361581+Volodymyr-Tym@users.noreply.github.com>
2024-09-28 16:22:32 +0100

    Make task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Description from './components/Description/Description';
 import Options from './components/Options/Options';
@@ -7,27 +7,39 @@ import Feedback from './components/Feedback/Feedback';
 import Notification from './components/Notification/Notification';
 
 function App() {
-  const [rating, setRating] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const initialRating = { good: 0, neutral: 0, bad: 0 };
+
+  const [rating, setRating] = useState(() => {
+    const fromStorageRating = JSON.parse(
+      window.localStorage.getItem('storage-rating')
+    );
+
+    return fromStorageRating ?? initialRating;
   });
+
+  useEffect(() => {
+    window.localStorage.setItem('storage-rating', JSON.stringify(rating));
+  }, [rating]);
 
   const updateFeedback = feedbackType => {
     setRating({ ...rating, [feedbackType]: rating[feedbackType] + 1 });
 
-    const reset = { good: 0, neutral: 0, bad: 0 };
-    feedbackType === 'reset' && setRating({ ...reset });
+    feedbackType === 'reset' && setRating({ ...initialRating });
   };
 
   const totalFeedback = rating.good + rating.neutral + rating.bad;
+  const positiveFeedback = Math.round((rating.good / totalFeedback) * 100);
 
   return (
     <div>
       <Description />
       <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
       {totalFeedback > 0 ? (
-        <Feedback feedbacks={rating}></Feedback>
+        <Feedback
+          feedbacks={rating}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        ></Feedback>
       ) : (
         <Notification></Notification>
       )}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,9 +1,11 @@
-const Feedback = ({ feedbacks }) => {
+const Feedback = ({ feedbacks, totalFeedback, positiveFeedback }) => {
   return (
     <ul>
       <li>Good: {feedbacks.good}</li>
       <li>Neutral: {feedbacks.neutral}</li>
       <li>Bad: {feedbacks.bad}</li>
+      <li>Total: {totalFeedback}</li>
+      <li>Positive: {positiveFeedback}%</li>
     </ul>
   );
 };
